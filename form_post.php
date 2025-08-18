2cecd30f070105afb2b8b199fb72bcfe7ee68555
Wdpf-66 <ccsl61588@gmail.com>
2025-08-18 12:24:14 +0600

    form_post

--- form_post.php
+++ form_post.php
@@ -1,23 +1,10 @@
 <?php
- $name = null;
- $errors=[];
-//  print_r($_GET);
- if(isset($_GET['btn_Name'])){
-    $name = $_GET['name'];
-    
-    if(empty( $name)){
-      $errors['name']= "Name field is Required";
-    }elseif(!preg_match("/^[a-zA-Z .]{2,}$/", $name)){
-         $errors['name']= "Valid Name is Required";
-    }
-   
+ if(isset($_POST['btn_Name'])){
+    $name = $_REQUEST['name'];
+     echo $name;
+  //  print_r($_POST);
+  //  print_r($_REQUEST);
  }
-
-  $errors["email"]= "Not a Valid Eamail";
-  $errors[0]= "index";
-
-  print_r($errors);
- 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -28,13 +15,13 @@
 </head>
 <body>
     <h1> This  is  PHP File</h1>
-     <form action="from.php" method="GET">
+     <form action="" method="POST">
        <label for="n">Give Your Name</label> <br>
        <input type="text" name="name" id="n"> <br> <br>
-        <?php if(!empty($errors["name"])) echo "<p>{$errors['name']}</p>"; ?>
+       
        <input type="submit" name="btn_Name" >
 
      </form>
-    <h2>My Name is <?php echo $name ;?> </h2>
+   
 </body>
 </html>
